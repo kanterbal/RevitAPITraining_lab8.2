953f4fa4a9b0164392a59521704783d2ce68a60b
sergey leonov <redfortten@hotmail.com>
2022-02-08 18:20:51 +0300

    Добавьте файлы проекта.

---
+++ RevitAPITraining_lab8.2/Main.cs
@@ -0,0 +1,29 @@
+﻿using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RevitAPITraining_lab8._2
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Main : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            Document doc = commandData.Application.ActiveUIDocument.Document;
+            var nwcOption = new NavisworksExportOptions();
+            using (var ts = new Transaction(doc, "export nwc"))
+            {
+                ts.Start();
+                doc.Export(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "export.nwc", nwcOption);
+                ts.Commit();
+            }
+
+            return Result.Succeeded;
+        }
+    }
+}
